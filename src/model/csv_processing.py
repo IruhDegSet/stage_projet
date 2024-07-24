6a507ff5f927e8f513f5ec810e0c73606136bd0e
khawla belgacem <k_belgacem@estin.dz>
2024-07-24 15:29:04 +0100

    .

---
+++ src/model/csv_processing.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+import os
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from utils_processing import load_documents, split_text, save_to_chroma
+
+load_dotenv()
+
+CHROMA_PATH = os.path.abspath(f"../{os.getenv('CHROMA_PATH')}")
+BATCH_SIZE = int(os.getenv('BATCH_SIZE'))  # Ajustez cette valeur selon votre système
+DATA_PATH = os.path.abspath(f"../{os.getenv('DATA_PATH_CSV')}")
+
+def generate_data_store():
+    try:
+        loader = CSVLoader(file_path=DATA_PATH, encoding='UTF-8')
+        documents = load_documents(loader)
+        if not documents:
+            return
+
+        chunks = split_text(documents)
+        if not chunks:
+            return
+
+        save_to_chroma(chunks, CHROMA_PATH, BATCH_SIZE)
+    except Exception as e:
+        print(f"An error occurred in generate_data_store: {e}")
+
+if __name__ == "__main__":
+    generate_data_store()
