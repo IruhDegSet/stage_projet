fd850a25c59ac099b2e15dd03f69942cd3205c6d
IruhDegSet <93375017+IruhDegSet@users.noreply.github.com>
2024-07-31 12:40:17 +0100

    Update streamlit_app.py

--- src/model/streamlit_app.py
+++ src/model/streamlit_app.py
@@ -22,6 +22,35 @@ BATCH_SIZE = 1000
 COLLECTION_CSV = 'csv_collection'
 MBD_MODEL = 'intfloat/multilingual-e5-large'
 
+import chromadb
+# Initialiser le client Chroma en mode lecture seule, si disponible
+db = chromadb.PersistentClient(path="../data/chroma")
+
+# Lister toutes les collections
+collections = db.list_collections()
+
+
+# Afficher la liste des collections
+print("Collections in Chroma DB:", collections)
+
+# Optionnel: afficher le nombre de collections
+print("Number of collections:", len(collections))
+if collections:
+    print("Collections in Chroma DB:")
+    for collection_name in collections:
+        print(f"Processing collection '{collection_name}'...")
+        
+else:
+    print("No collections found in Chroma DB.")
+
+csv = db.get_or_create_collection("csv_collection")
+
+txt = db.get_or_create_collection("txt_collection")
+
+
+st.write("There are csv", csv.count(), "in the collection")
+st.write("There are txt", txt.count(), "in the collection")
+
 def inspect_retriever(query: str, k: int = 10):
     persist_directory = CHROMA_PATH
     embedding = HuggingFaceInferenceAPIEmbeddings(api_key=API_TOKEN, model_name=MBD_MODEL)
