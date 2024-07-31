baa41b6b6d5aac6cd3ad2160e93add3496dac2cf
IruhDegSet <93375017+IruhDegSet@users.noreply.github.com>
2024-07-31 14:40:33 +0100

    Update streamlit_app.py

--- src/model/streamlit_app.py
+++ src/model/streamlit_app.py
@@ -1,6 +1,3 @@
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 try:
     from langchain_community.document_loaders.csv_loader import CSVLoader
@@ -10,11 +7,9 @@ except ImportError:
 from langchain_groq import ChatGroq
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
-<<<<<<< HEAD
 from langchain_qdrant import QdrantVectorStore as qd
-=======
 from langchain_community.vectorstores import Chroma
->>>>>>> 3c65813bc94dfe3d694643c400c2a8305d89c3f6
+
 from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 
 # Constants
@@ -26,10 +21,6 @@ BATCH_SIZE = 1000
 COLLECTION_CSV = 'csv_collection'
 MBD_MODEL = 'intfloat/multilingual-e5-large'
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 3c65813bc94dfe3d694643c400c2a8305d89c3f6
 def ask_bot(query: str, k: int = 10):
     embeddings = HuggingFaceInferenceAPIEmbeddings(api_key=API_TOKEN, model_name=MBD_MODEL)
     vectordb = qd.from_existing_collection(embedding=embeddings,
@@ -57,23 +48,19 @@ def ask_bot(query: str, k: int = 10):
         chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
     )
 
-<<<<<<< HEAD
     
     # Run chain:
     result = qa_chain.invoke({"query": query})
     # st.write(result) 
-=======
+
     # Run chain:
     result = qa_chain.invoke({"query": query})
->>>>>>> 3c65813bc94dfe3d694643c400c2a8305d89c3f6
     return result['result']
 
 st.title('DGF Product Seeker Bot')
 query = st.chat_input("Qu'est ce que vous cherchez? Ex: Laptop avec 16gb de ram")
 if query:
-<<<<<<< HEAD
+
     # inspect_retriever(query)
-=======
->>>>>>> 3c65813bc94dfe3d694643c400c2a8305d89c3f6
     answer = ask_bot(query)
     st.markdown(answer)
