b763aa84700459981c68fadab0993d7965eff8df
khawla belgacem <k_belgacem@estin.dz>
2024-08-09 13:51:31 +0100

    ..

--- src/model/streamlit_app.py
+++ src/model/streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import uuid
 from langchain_community.document_loaders.csv_loader import CSVLoader
 from langchain.chains import RetrievalQA
 from langchain_groq import ChatGroq
@@ -6,6 +7,14 @@ from langchain.prompts import PromptTemplate
 from langchain_qdrant import QdrantVectorStore as qd
 from langchain.memory import ConversationBufferMemory
 from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_chroma import Chroma
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 
 # Constants
 API_TOKEN = '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'
@@ -14,16 +23,19 @@ CHROMA_PATH = "../data/chroma"
 COLLECTION_CSV = 'csv_collection'
 MBD_MODEL = 'intfloat/multilingual-e5-large'
 
+def initialize_store():
+    if 'store' not in st.session_state:
+        st.session_state.store = {}
 
-def ask_bot(question: str, k: int = 10):
-    st.write('query:', question)
-    # Initialisation de la mémoire
-    memory = ConversationBufferMemory(
-        input_key='query',
-        memory_key="history",
-    )
+def get_session_id():
+    if 'session_id' not in st.session_state:
+        st.session_state.session_id = str(uuid.uuid4())
+    return st.session_state.session_id
 
-    # Configuration des embeddings et de la base de données
+def ask_bot(query: str, k: int = 10):
+    initialize_store()
+    session_id = get_session_id()
+    
     embeddings = HuggingFaceInferenceAPIEmbeddings(api_key=API_TOKEN, model_name=MBD_MODEL)
     vectordb = qd.from_existing_collection(
         embedding=embeddings,
@@ -33,81 +45,84 @@ def ask_bot(question: str, k: int = 10):
         collection_name="lvHP_collection",
         vector_name=''
     )
-
-    # Configuration du modèle et du prompt
+    
     llm = ChatGroq(model_name='llama-3.1-70b-versatile', api_key=GROQ_TOKEN, temperature=0)
+   
+    retriever = vectordb.as_retriever(search_type='mmr', search_kwargs={'k': 50, 'fetch_k': k})
+
+    contextualize_q_system_prompt = (
+        "Étant donné un historique de conversation et la dernière question de l'utilisateur "
+        "qui pourrait faire référence au contexte de l'historique de conversation, "
+        "formulez une question autonome qui peut être comprise "
+        "sans l'historique de conversation. NE RÉPONDEZ PAS à la question, "
+        "reformulez-la si nécessaire et, sinon, renvoyez-la telle quelle."
+    )
+
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    history_aware_retriever = create_history_aware_retriever(
+        llm, retriever, contextualize_q_prompt
+    )
+
     template = """
-                    Tu es un assistant vendeur. Tu as accès au contexte seulement. Ne génère pas des informations si elles ne sont pas dans le contexte. 
-                    Répond seulement si tu as la réponse. Affiche les produits un par un sous forme de tableau qui contient ces colonne Référence,Categorie, Marque, Description.
-                    Il faut savoir que laptop, ordinateur, ordinateurs portable , pc et poste de travail ont tous le même sens.
-                    Il faut savoir que téléphone portable et smartphone ont le même sens.
-                    Il faut savoir que tout autre caractéristique du produit tel que la RAM stockage font partie de la description du produit et il faut filtrer selon la marque et la catégorie seulement.
-                
-                    Si le contexte est vide, dis-moi que tu n'as pas trouvé de produits correspondants. Je veux que la réponse soit claire et facile à lire, avec des sauts de ligne pour séparer chaque produit. Ne me donne pas de produits qui ne sont pas dans le contexte.
-                    si je te pose une question sur les question ou les reponses fournients précédemment tu dois me répondre selon l'historique.
-                    tu ne dois pas oublier l'historique car parfois le user continue à te poser des questions sur tes réponses que tu as déjà fournies auparavant
-    
-                    Contexte: {context}
-                    Historique : {history}
-                    Question: {query}
+        Tu es un assistant vendeur. Tu as accès au contexte seulement. Ne génère pas des informations si elles ne sont pas dans le contexte. 
+        Répond seulement si tu as la réponse. Affiche les produits un par un sous forme de tableau qui contient ces colonnes Référence, Categorie, Marque, Description.
+        Il faut savoir que laptop, ordinateur, ordinateurs portables, pc et poste de travail ont tous le même sens.
+        Il faut savoir que téléphone portable et smartphone ont le même sens.
+        Tout autre caractéristique du produit tel que la RAM et le stockage font partie de la description du produit et il faut filtrer selon la marque et la catégorie seulement.
+        Si le contexte est vide, dis-moi que tu n'as pas trouvé de produits correspondants. Je veux que la réponse soit claire et facile à lire, avec des sauts de ligne pour séparer chaque produit. Ne me donne pas de produits qui ne sont pas dans le contexte.
+        Si je te pose une question sur les questions ou les réponses fournies précédemment, tu dois me répondre selon l'historique.
+        Tu ne dois pas oublier l'historique car parfois l'utilisateur continue à te poser des questions sur tes réponses que tu as déjà fournies auparavant.
+        Contexte: {context}
+        Réponse :
+    """
 
-                    Réponse :
-                    """
-  
-    prompt = PromptTemplate(
-        input_variables=["context", "history", "query"],
-        template=template,
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", template),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+    def get_session_history(session_id: str) -> BaseChatMessageHistory:
+        if session_id not in st.session_state.store:
+            st.session_state.store[session_id] = ChatMessageHistory()
+        return st.session_state.store[session_id]
+
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+        output_messages_key="answer",
     )
 
-    chain_type_kwargs = {
-        "prompt": prompt,
-        "memory": memory,
-    }
-    
-    qa = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type='stuff',
-        retriever=vectordb.as_retriever(search_type='mmr', search_kwargs={'k': 50, 'fetch_k': k}),
-        verbose=True,
-        memory=memory,
-        chain_type_kwargs=chain_type_kwargs
+    st.write(f"Session ID: {session_id}")
+    st.write("Store Before:", st.session_state.store)
+
+    response = conversational_rag_chain.invoke(
+        {"input": query},
+        config={
+            "configurable": {"session_id": session_id}
+        },
     )
-    st.write(qa)
-    # Chargement de l'historique
-    conversation_history = memory.load_memory_variables({})
-    history = conversation_history.get('history', "")
 
-    # Préparation des inputs avec les clés correctes
-    inputs = {
-        "context": "",  # Contexte vide par défaut
-        "history": history,
-        "query": question  # Utilisation de 'query' comme spécifié dans le PromptTemplate
-    }
+    st.write("Store After:", st.session_state.store)
+    st.write("Response:", response["answer"])
 
-    st.write("Inputs fournis à la chaîne:", inputs)
-    
-    # Appel à la chaîne avec les clés appropriées
-    try:
-        result = qa.invoke(inputs)
-    except ValueError as e:
-        st.error(f"Erreur de valeur : {e}")
-        return 'Erreur lors de l\'appel de la chaîne.', []
-
-    # Gestion des résultats
-    output = result.get('result', 'Pas de réponse trouvée')
-    sources = result.get('source_documents', [])
-
-    # Mise à jour de la mémoire
-    memory.save_context({'query': question}, {'result': output})
-
-    return output, sources
+    return response["answer"]
 
 st.title('DGF Product Seeker Bot')
 query = st.chat_input("Qu'est-ce que vous cherchez ? Ex : Laptop avec 16 Go de RAM")
 if query:
-    answer, sources = ask_bot(query)
+    answer = ask_bot(query)
     st.markdown(answer)
-    if sources:
-        st.write("Sources :")
-        for doc in sources:
-            st.write(doc)
